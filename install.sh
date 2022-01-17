febd01dde2fa0a9160e3608a1dda566879f7060f
30p87 <50525850+30p87@users.noreply.github.com>
2022-01-17 14:13:24 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt install -y python3 npm nodejs mpv
+sudo pacman -Syu python3 npm mpv
+
+python -m pip install -U pip
+python -m pip install -U requests
+
+npm install webtorrent-cli -g || sudo npm install webtorrent-cli -g || echo "npm is a whiny bitch again, try installing webtorrent-cli manually"
+
+curl https://raw.githubusercontent.com/30p87/notflix.py/main/notflix.py | sudo tee /bin/notflix.py
