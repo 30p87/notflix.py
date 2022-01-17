12f30cf7af0461dacb96d776b03ab4e1f616f16a
30p87 <50525850+30p87@users.noreply.github.com>
2022-01-17 14:16:57 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 sudo apt install -y python3 npm nodejs mpv
 sudo pacman -Syu python3 npm mpv
 
-python -m pip install -U pip
-python -m pip install -U requests
+python3 -m pip install -U pip
+python3 -m pip install -U requests
 
 npm install webtorrent-cli -g || sudo npm install webtorrent-cli -g || echo "npm is a whiny bitch again, try installing webtorrent-cli manually"
 
