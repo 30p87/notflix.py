1b1a7ebc885883c30c84d6cd80f34d351b130891
30p87 <50525850+30p87@users.noreply.github.com>
2022-01-21 20:10:40 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
-Invoke-WebRequest -Uri https://www.python.org/ftp/python/3.10.2/python-3.10.2-amd64.exe -OutFile $Env:USERPROFILE\AppData\Local\Temp\python_install.exe
-Invoke-WebRequest -Uri https://nodejs.org/dist/v17.4.0/node-v17.4.0-x64.msi -OutFile $Env:USERPROFILE\AppData\Local\Temp\nodejs_install.msi
-Invoke-WebRequest -Uri https://www.7-zip.org/a/7z2107-x64.exe -OutFile $Env:USERPROFILE\AppData\Local\Temp\7z_install.exe
-Invoke-WebRequest -Uri https://netcologne.dl.sourceforge.net/project/mpv-player-windows/64bit/mpv-x86_64-20220116-git-1ba0547.7z -OutFile $Env:USERPROFILE\AppData\Local\Temp\mpv_install.7z
+Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.10.2/python-3.10.2-amd64.exe" -OutFile $Env:USERPROFILE\AppData\Local\Temp\python_install.exe
+Invoke-WebRequest -Uri "https://nodejs.org/dist/v17.4.0/node-v17.4.0-x64.msi" -OutFile $Env:USERPROFILE\AppData\Local\Temp\nodejs_install.msi
+Invoke-WebRequest -Uri "https://www.7-zip.org/a/7z2107-x64.exe" -OutFile $Env:USERPROFILE\AppData\Local\Temp\7z_install.exe
+Invoke-WebRequest -Uri "https://netcologne.dl.sourceforge.net/project/mpv-player-windows/64bit/mpv-x86_64-20220116-git-1ba0547.7z" -OutFile $Env:USERPROFILE\AppData\Local\Temp\mpv_install.7z
 
 & "$Env:USERPROFILE\AppData\Local\Temp\python_install.exe" /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 msiexec /qn /l* C:\node-log.txt /i $Env:USERPROFILE\AppData\Local\Temp\nodejs_install.msi
@@ -10,11 +10,22 @@ msiexec /qn /l* C:\node-log.txt /i $Env:USERPROFILE\AppData\Local\Temp\nodejs_in
 
 py -m pip install requests
 
-mkdir $Env:AppData\notflix.py
+If ((Test-Path "$Env:AppData\notflix.py") -eq $False) {
+  mkdir $Env:AppData\notflix.py
+}
 
 cp $Env:USERPROFILE\AppData\Local\Temp\mpv_install\mpv.exe $Env:AppData\notflix.py
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/30p87/notflix.py/windows-(beta)/notflix.py -OutFile $Env:AppData\notflix.py\notflix.py
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/30p87/notflix.py/windows-(beta)/notflix.py" -OutFile $Env:AppData\notflix.py\notflix.py
 
-$oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
-$newpath = "$oldpath;$Env:AppData\notflix.py"
-Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newpath
+$is = $False
+foreach ($_path in ($Env:PATH).split(";")) {
+  If ("$_path" -match "notflix.py") {
+    $is = $True
+  }
+}
+
+If ($is -eq $False) {
+  $oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
+  $newpath = "$oldpath;$Env:AppData\notflix.py"
+  Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newpath
+}
