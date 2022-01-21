8fc454035f5e9d9d7411f9cec25073025c9b38fb
30p87 <50525850+30p87@users.noreply.github.com>
2022-01-21 19:21:02 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -3,7 +3,7 @@ Invoke-WebRequest -Uri https://nodejs.org/dist/v17.4.0/node-v17.4.0-x64.msi -Out
 Invoke-WebRequest -Uri https://www.7-zip.org/a/7z2107-x64.exe -OutFile $Env:USERPROFILE\AppData\Local\Temp\7z_install.exe
 Invoke-WebRequest -Uri https://netcologne.dl.sourceforge.net/project/mpv-player-windows/64bit/mpv-x86_64-20220116-git-1ba0547.7z -OutFile $Env:USERPROFILE\AppData\Local\Temp\mpv_install.7z
 
-Invoke-Expression "$Env:USERPROFILE\AppData\Local\Temp\python_install.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0"
+& "$Env:USERPROFILE\AppData\Local\Temp\python_install.exe" /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 msiexec /qn /l* C:\node-log.txt /i $Env:USERPROFILE\AppData\Local\Temp\nodejs_install.msi
-Invoke-Expression "$Env:USERPROFILE\AppData\Local\Temp\7z_install.exe /S"
-Invoke-Expression "$env:ProgramFiles\7-Zip\7z.exe x -o
+& "$Env:USERPROFILE\AppData\Local\Temp\7z_install.exe" /S
+& "$env:ProgramFiles\7-Zip\7z.exe" x -o"$Env:USERPROFILE\AppData\Local\Temp\mpv_install" $Env:USERPROFILE\AppData\Local\Temp\mpv_install.7z -r
