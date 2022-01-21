49b4eab9bd2f9884f22fafe40030982d080a9290
30p87 <50525850+30p87@users.noreply.github.com>
2022-01-21 17:26:18 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,4 +1,8 @@
-echo "test"
+Invoke-WebRequest -Uri https://www.python.org/ftp/python/3.10.2/python-3.10.2-amd64.exe -OutFile $Env:USERPROFILE\AppData\Local\Temp\python_install.exe
+Invoke-WebRequest -Uri https://nodejs.org/dist/v17.4.0/node-v17.4.0-x64.msi -OutFile $Env:USERPROFILE\AppData\Local\Temp\nodejs_install.msi
+
+Invoke-Expression "$Env:USERPROFILE\AppData\Local\Temp\test.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0"
+msiexec /qn /l* C:\node-log.txt /i $Env:USERPROFILE\AppData\Local\Temp\nodejs_install.msi
 
 <#
 sudo apt install -y python3 npm nodejs mpv
