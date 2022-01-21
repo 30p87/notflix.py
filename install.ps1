213ecf1d070e7bbef707815daccf11ce2b2a7c78
30p87 <50525850+30p87@users.noreply.github.com>
2022-01-21 20:51:56 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -3,10 +3,13 @@ Invoke-WebRequest -Uri "https://nodejs.org/dist/v17.4.0/node-v17.4.0-x64.msi" -O
 Invoke-WebRequest -Uri "https://www.7-zip.org/a/7z2107-x64.exe" -OutFile $Env:USERPROFILE\AppData\Local\Temp\7z_install.exe
 Invoke-WebRequest -Uri "https://netcologne.dl.sourceforge.net/project/mpv-player-windows/64bit/mpv-x86_64-20220116-git-1ba0547.7z" -OutFile $Env:USERPROFILE\AppData\Local\Temp\mpv_install.7z
 
-& "$Env:USERPROFILE\AppData\Local\Temp\python_install.exe" /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+Start-Process "$Env:USERPROFILE\AppData\Local\Temp\python_install.exe" "/quiet InstallAllUsers=1 PrependPath=1 Include_test=0" -wait
 msiexec /qn /l* C:\node-log.txt /i $Env:USERPROFILE\AppData\Local\Temp\nodejs_install.msi
-& "$Env:USERPROFILE\AppData\Local\Temp\7z_install.exe" /S
-& "$env:ProgramFiles\7-Zip\7z.exe" x -o"$Env:USERPROFILE\AppData\Local\Temp\mpv_install" $Env:USERPROFILE\AppData\Local\Temp\mpv_install.7z -r
+Start-Process "$Env:USERPROFILE\AppData\Local\Temp\7z_install.exe" "/S" -wait
+If ((Test-Path "$Env:USERPROFILE\AppData\Local\Temp\mpv_install") -eq $True) {
+  Remove-Item "$Env:USERPROFILE\AppData\Local\Temp\mpv_install" -Force -Recurse
+}
+Start-Process "$env:ProgramFiles\7-Zip\7z.exe" "x -o$Env:USERPROFILE\AppData\Local\Temp\mpv_install $Env:USERPROFILE\AppData\Local\Temp\mpv_install.7z -r" -wait
 
 py -m pip install requests
 
@@ -29,3 +32,4 @@ If ($is -eq $False) {
   $newpath = "$oldpath;$Env:AppData\notflix.py"
   Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newpath
 }
+"
