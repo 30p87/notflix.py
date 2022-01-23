abcdf1e0b10519d61c773a26019e1d872558ad9c
30p87 <50525850+30p87@users.noreply.github.com>
2022-01-23 15:13:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ pip3 install -U requests
 
 # try to install python and mpv via apt : try to install python, mpv and npm via pacman, as it's probably arch based and the fix version 17 "fix" is not needed
 
-curl https://raw.githubusercontent.com/30p87/notflix.py/macos-(beta)/notflix.py | sudo tee /usr/local/bin/notflix.py
+curl "https://raw.githubusercontent.com/30p87/notflix.py/macos-(beta)/notflix.py" | sudo tee /usr/local/bin/notflix.py
 # get the newest version of the script from the linxu branch of the repo, then tee it into PATH
 sudo chmod +x /usr/local/bin/notflix.py
 # make the file executable, so you can enter notflix.py everywhere
